926b78727c32ff03b3369cd9344997d506945950
Prateek <prateekgurnani10@gmail.com>
2019-07-18 04:28:23 -0500

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,9 @@ class App extends React.Component {
     const { params } = this.props.match;
     const localStorageRef = localStorage.getItem(params.storeId);
     if (localStorageRef) {
-      this.setState({ order: JSON.parse(localStorageRef) });
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
     }
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
@@ -33,39 +35,58 @@ class App extends React.Component {
   }
 
   addFish = fish => {
-    const fishes = { ...this.state.fishes };
+    const fishes = {
+      ...this.state.fishes
+    };
     fishes[`fish${Date.now()}`] = fish;
 
-    this.setState({ fishes });
+    this.setState({
+      fishes
+    });
   };
   updateFish = (key, updatedFish) => {
-    const fishes = { ...this.state.fishes };
+    const fishes = {
+      ...this.state.fishes
+    };
     fishes[key] = updatedFish;
-    this.setState({ fishes });
+    this.setState({
+      fishes
+    });
   };
 
   deleteFish = key => {
-    const fishes = { ...this.state.fishes };
+    const fishes = {
+      ...this.state.fishes
+    };
 
     fishes[key] = null;
 
-    this.setState({ fishes });
+    this.setState({
+      fishes
+    });
   };
   loadSampleFishes = () => {
-    this.setState({ fishes: sampleFishes });
+    this.setState({
+      fishes: sampleFishes
+    });
   };
   addToOrder = key => {
-    const order = { ...this.state.order };
+    const order = {
+      ...this.state.order
+    };
     order[key] = order[key] + 1 || 1;
-    this.setState({ order });
+    this.setState({
+      order
+    });
   };
 
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Cajun Shit" />
+          <Header tagline="Fresh Louisiana Seafood" />
           <ul className="fishes">
+            {" "}
             {Object.keys(this.state.fishes).map(key => (
               <Fish
                 key={key}
@@ -73,17 +94,17 @@ class App extends React.Component {
                 details={this.state.fishes[key]}
                 addToOrder={this.addToOrder}
               />
-            ))}
-          </ul>
-        </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+            ))}{" "}
+          </ul>{" "}
+        </div>{" "}
+        <Order fishes={this.state.fishes} order={this.state.order} />{" "}
         <Inventory
           addFish={this.addFish}
           updateFish={this.updateFish}
           deleteFish={this.deleteFish}
           loadSampleFishes={this.loadSampleFishes}
           fishes={this.state.fishes}
-        />
+        />{" "}
       </div>
     );
   }
